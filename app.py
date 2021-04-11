00735aa7acda639f48ffdea3827e39c0211f5e36
OitaGG <oitag@yandex.ru>
2021-04-11 22:17:32 +0300

    natasha dlya nas shik

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 from flask import Flask, request
-import spacy
-from spacy.tokenizer import Tokenizer
+from natasha import NewsNERTagger, Segmenter, NewsEmbedding, Doc
 
 app = Flask(__name__)
-nlp = spacy.load('ru2')
-tokenizer = Tokenizer(nlp.vocab)
+segmenter = Segmenter()
+emb = NewsEmbedding()
+ner_tagger = NewsNERTagger(emb)
 
 
 @app.route('/anonymize', methods=["POST"])
 def anonymoize():
     entities = request.json['entities']
     raw_text = request.json['raw_text']
-    doc = nlp(raw_text)
-    print([X for X in doc.ents])
-    return " ".join(
-        map(lambda p: filter_data(p, entities),
-            [(X, X.ent_type_) for X in doc]))
+    doc = Doc(raw_text)
+    doc.segment(segmenter)
+    doc.tag_ner(ner_tagger)
+    return " ".join(map(lambda p: filter_data(p, entities), doc.sents))
 
 
-def filter_data(tuple_data, entities):
-    if (str(tuple_data[1]) in entities and str(tuple_data[1]) != ""):
-        return "***"
-    return str(tuple_data[0])
+def filter_data(sents, entities):
+    text = sents.text
+    spans = sents.spans
+    for span in spans:
+        if (span.type in entities):
+            text = "".join([
+                text[:span.start], "*" * (span.stop - span.start),
+                text[span.stop:]
+            ])
+    return text
 
 
 if __name__ == '__main__':
