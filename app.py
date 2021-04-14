a1ea1f19eae5120e1f225f00e77f02c5192beed3
OitaGG <oitag@yandex.ru>
2021-04-14 18:44:44 +0300

    add date extractor

--- app.py
+++ app.py
@@ -1,31 +1,45 @@
 from flask import Flask, request
-from natasha import NewsNERTagger, Segmenter, NewsEmbedding, Doc
+from natasha import NewsNERTagger, Segmenter, NewsEmbedding, Doc, MorphVocab, DatesExtractor
 
 app = Flask(__name__)
+
+# ner
 segmenter = Segmenter()
 emb = NewsEmbedding()
 ner_tagger = NewsNERTagger(emb)
+
+# for dates extracting
+morph_vocab = MorphVocab()
+dates_extractor = DatesExtractor(morph_vocab)
 
 
 @app.route('/anonymize', methods=["POST"])
 def anonymoize():
     entities = request.json['entities']
     raw_text = request.json['raw_text']
+    if "DATE" in entities:
+        raw_text = anonymize_date(raw_text)
     doc = Doc(raw_text)
     doc.segment(segmenter)
     doc.tag_ner(ner_tagger)
-    return " ".join(map(lambda p: filter_data(p, entities), doc.sents))
+    return filter_data(doc.spans, raw_text, entities)
 
 
-def filter_data(sents, entities):
-    text = sents.text
-    spans = sents.spans
+def anonymize_data(span, text):
+    return "".join(
+        [text[:span.start], "*" * (span.stop - span.start), text[span.stop:]])
+
+
+def filter_data(spans, raw_text, entities):
     for span in spans:
         if (span.type in entities):
-            text = "".join([
-                text[:span.start], "*" * (span.stop - span.start),
-                text[span.stop:]
-            ])
+            raw_text = anonymize_data(span, raw_text)
+    return raw_text
+
+
+def anonymize_date(text):
+    for span in list(dates_extractor(text)):
+        text = anonymize_data(span, text)
     return text
 
 
