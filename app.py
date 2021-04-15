bbbbc3925ddb7417849d4fcbb964240122ea34c6
alex <alexander.korichev@gmail.com>
2021-04-15 19:58:48 +0300

    freeze reqs + add front

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from natasha import NewsNERTagger, Segmenter, NewsEmbedding, Doc, MorphVocab, DatesExtractor
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='build/')
 
 # ner
 segmenter = Segmenter()
